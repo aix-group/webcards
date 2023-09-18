f0b21fbc91abaab1cfde79ed30a53b9775ff0a33
Bahadir <115642717+bahadirery@users.noreply.github.com>
2023-09-18 14:03:39 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,3 +10,19 @@ sys.path.insert(0, os.path.abspath('.'))
 project = 'WebCards'
 copyright = '2023'
 author = 'Bahadir Eryilmaz'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
