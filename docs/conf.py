a6b99a7b114d1f9629629a68bb4c318654ac270f
Bahadir <115642717+bahadirery@users.noreply.github.com>
2023-09-18 13:43:10 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'WebCards'
+copyright = '2023'
+author = 'Bahadir Eryilmaz'
